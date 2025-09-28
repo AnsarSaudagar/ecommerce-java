96779908ed19d1f4ed7851bc81188d49d5dd9f07
ansar <ansarsaudagar40@gmail.com>
2025-09-28 18:11:12 +0530

    first commit

---
+++ src/main/java/com/example/ecommerce/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.example.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("")
+    public Object test(){
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("ansar", 1);
+        return data;
+    }
+}
